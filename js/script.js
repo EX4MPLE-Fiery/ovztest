4a41fa1134dc94f857eb9a98f8b5a5b0ae22f6ba
EX4MPLE-Fiery <64203759+EX4MPLE-Fiery@users.noreply.github.com>
2020-04-26 04:13:44 +0600

    обновление базы вопросов (60+)

---
+++ js/script.js
@@ -0,0 +1,243 @@
+document.getElementById('search_button').onclick =
+    function() {
+    var val = document.getElementById('question').value;
+    if(val == "Дөңгелектіктен ауытқу базалары болып есептеледі.") {
+        document.getElementById('answer').innerHTML="Ответ: Жанасатын шеңбер";
+      }
+
+    else if(val == "6 - дәлдік классты подшипникті айналатын біліктерге қондыру шек өрісі") {
+          document.getElementById('answer').innerHTML="Ответ: n6, m6, k6,js6";
+    }
+
+    else if(val == "Тесік жүйесіндегі өтпелі қондыруда біліктің шек өрісін белгіленуі") {
+          document.getElementById('answer').innerHTML="Ответ: js, k, m, n";
+    }
+
+    else if(val == "Жұмысшы скобаның номинальды өлшемі тең...") {
+          document.getElementById('answer').innerHTML="Ответ: Біліктің ең үлкен шекті өлшеміне.";
+    }
+
+    else if(val == "Жұмысшы калибрлермен анықтау мүмкін") {
+          document.getElementById('answer').innerHTML="Ответ: Нақты өлшемдер жарамды ма немесе белгіленген шектен шыға ма";
+    }
+
+    else if(val == "Шартты көрсетілу белгісі  Ç") {
+          document.getElementById('answer').innerHTML="Ответ: Берілген профиль пішін шақтамасы";
+    }
+
+    else if(val == "Подшипниктің дәлдік классын көрсету") {
+          document.getElementById('answer').innerHTML="Ответ: 6-205";
+    }
+
+    else if(val == "Тесік жүйесіндегі өтпелі қондыруда біліктің шек өрісін белгіленуі") {
+          document.getElementById('answer').innerHTML="Ответ: js, k, m, n";
+    }
+
+    else if(val == "Тістердің түйісу сипаты немен анықталады?") {
+          document.getElementById('answer').innerHTML="Ответ: Тістер арасындағы бүйір саңылаумен.";
+    }
+
+    else if(val == "Нақты өлшем дегеніміз:") {
+          document.getElementById('answer').innerHTML="Ответ: Мүмкіндік ақаумен әзірленген тетік өлшемі ИЛИ Белгіленген мүмкіндік қателікпен өлшенген өлшем";
+    }
+
+    else if(val == "Төменгі шекті ауытқу дегеніміз:") {
+          document.getElementById('answer').innerHTML="Ответ: Ең кіші шекті және номинальды өлшемдердің алгебралық айырмашылғы";
+    }
+
+    else if(val == "Ең кіші керіліс") {
+          document.getElementById('answer').innerHTML="Ответ: Nmin=dmin-Dmax";
+    }
+
+    else if(val == "Қондыру дегеніміз не?") {
+          document.getElementById('answer').innerHTML="Ответ: Саңылау және керіліс шамасын алумен анықталатын тетіктерді қосу сипаты.";
+    }
+
+    else if(val == "Төменгі шекті ауытқу дегеніміз:") {
+          document.getElementById('answer').innerHTML="Ответ: Ең кіші шекті және номинальды өлшемдердің алгебралық айырмашылғы";
+    }
+
+    else if(val == "Гайка ішкі диаметрінің дәлдік дәрежесі") {
+          document.getElementById('answer').innerHTML="Ответ: 4,5,6,7,8.";
+    }
+
+    else if(val == "Метрлі бұранда дәлдік классы") {
+          document.getElementById('answer').innerHTML="Ответ: Дәл, орташа, дөрекі";
+    }
+
+    else if(val == "Өлшем шақтамасы") {
+          document.getElementById('answer').innerHTML="Ответ: Ең жоғарғы және ең кіші шекті өлшемдер айырмашылығы";
+    }
+
+    else if(val == "Жұмысшы калибрлермен анықтау мүмкін") {
+          document.getElementById('answer').innerHTML="Ответ: Нақты өлшемдер жарамды ма немесе белгіленген шектен шыға ма";
+    }
+
+    else if(val == "Техникалық өлшеуде дәлдік дәреженің атауы:") {
+          document.getElementById('answer').innerHTML="Ответ: Дәлдік квалитеттері.";
+    }
+
+    else if(val == "Өлшем шақтамасы") {
+          document.getElementById('answer').innerHTML="Ответ: Ең жоғарғы және ең кіші шекті өлшемдер айырмашылығы ";
+    }
+
+    else if(val == "Гайка ішкі диаметрінің дәлдік дәрежесі") {
+          document.getElementById('answer').innerHTML="Ответ: 4,5,6,7,8.";
+    }
+
+    else if(val == "М12-6g бұранда шек өрісін белгілеуде, көрсетілген") {
+          document.getElementById('answer').innerHTML="Ответ: d  және  d2  дәлдік дәрежесі 6";
+    }
+
+    else if(val == "Бұранда шек өрісін М12-5Н6Н белгілегенде көрсетіледі:") {
+          document.getElementById('answer').innerHTML="Ответ: 5 дәлдік дәрежесі  D2 және 6 дәлдік дәрежесі  D1.";
+    }
+
+    else if(val == "Дөңгелектіктен ауытқу базалары болып есептеледі:") {
+          document.getElementById('answer').innerHTML="Ответ: Жанасатын шеңбер.";
+    }
+
+    else if(val == "Жұмысшы калибрлермен анықтау мүмкін") {
+          document.getElementById('answer').innerHTML="Ответ: Нақты өлшемдер жарамды ма немесе белгіленген шектен шыға ма";
+    }
+
+    else if(val == "Шпонкалы қосылыстардың келесі түрлері белгіленген:") {
+          document.getElementById('answer').innerHTML="Ответ: Тығыз, нормальды, бос.";
+    }
+
+    else if(val == "Қондырулардың үш тобы:") {
+          document.getElementById('answer').innerHTML="Ответ: Саңылаумен қондыру.";
+    }
+
+    else if(val == "Калибрлер жұмысшы беттер кедір-бұдырлығы неге байланысты белгіленеді?") {
+          document.getElementById('answer').innerHTML="Ответ: Бақыланатын тетік беттер кедір-бұдырлығымен";
+    }
+
+    else if(val == "Подшипник сақинасын керіліспен жинақтайды, яғни") {
+          document.getElementById('answer').innerHTML="Ответ: Циркуляциялы салмақтауға сынайды";
+    }
+
+    else if(val == "Шартты белгілену белгісі ЦӘ") {
+          document.getElementById('answer').innerHTML="Ответ: Цилинидрлік шақтамасы.";
+    }
+
+    else if(val == "Бұранда шек өрісін М12-5Н6Н белгілегенде көрсетіледі:") {
+          document.getElementById('answer').innerHTML="Ответ: 5 дәлдік дәрежесі  D2 және 6 дәлдік дәрежесі  D1.";
+    }
+
+    else if(val == "Қондыру шақтамасы тең:") {
+          document.getElementById('answer').innerHTML="Ответ: Если ответы чисто: Smax-Smin или же: ТD + Td";
+    }
+
+    else if(val == "Шлицті қосылыстарды центрлеу тәсілдері:") {
+          document.getElementById('answer').innerHTML="Ответ: Ішкі диаметр бойынша.";
+    }
+
+    else if(val == "Жазықтықтан ауытқудың жеке түрлері:") {
+          document.getElementById('answer').innerHTML="Ответ: Дөңестік және ойыстық.";
+    }
+
+    else if(val == "Подшипник сақинасың саңылаумен жинақтайды, яғни") {
+          document.getElementById('answer').innerHTML="Ответ: Жерлік салмақтауға сынайды";
+    }
+
+    else if(val == "Калибрлер жұмысшы беттер кедір-бұдырлығы неге байланысты белгіленеді?") {
+          document.getElementById('answer').innerHTML="Ответ: Бақыланатын тетік беттер кедір-бұдырлығымен";
+    }
+
+    else if(val == "Шлицті қосылыстарды центрелеу әдісі.") {
+          document.getElementById('answer').innerHTML="Ответ: Ішкі, сыртқы диаметр және тістің бүйір беттері бойынша.";
+    }
+
+    else if(val == "Шартты белгілену белгісі  //") {
+          document.getElementById('answer').innerHTML="Ответ: Параллельдік шақтамасы";
+    }
+
+    else if(val == "Өзараауыстырымдылық келесі түрлерге бөлінеді:") {
+          document.getElementById('answer').innerHTML="Ответ: Толық және толық емес.";
+    }
+
+    else if(val == "Бұранда қадамы P") {
+          document.getElementById('answer').innerHTML="Ответ: Бұранда осіне паралелль бағытта өлшенген көрші біратты бүйір жақтар арасындағы қашықтық";
+    }
+
+    else if(val == "Өзараауыстырымдылықты қамтамасыз ететін факторлар:") {
+          document.getElementById('answer').innerHTML="Ответ: Стандарттарды пайдалану және сақтау.";
+    }
+
+    else if(val == "Шартты белгілену белгісі ЦІ") {
+          document.getElementById('answer').innerHTML="Ответ: Осьтестік шақтамасы.";
+    }
+
+    else if(val == "Бұранданың ішкі диаметрі – бұл елестетілген цилиндр диаметр, жанасқан") {
+          document.getElementById('answer').innerHTML="Ответ: Ішкі бұранда биіктігіне немесе сыртқы бұранда ойпаңдарына жанасқан.";
+    }
+
+    else if(val == "Тесік өлшемдері келесідей белгіленеді:") {
+          document.getElementById('answer').innerHTML="Ответ: A, B.";
+    }
+
+    else if(val == "Саңылаумен қондырылған білік шек өрісі") {
+          document.getElementById('answer').innerHTML="Ответ: a6, d11, f7, g8";
+    }
+
+    else if(val == "Тісті доңғалақтар эксплуатациялық міндеттері бойынша бөлінеді:") {
+          document.getElementById('answer').innerHTML="Ответ: Ауыр жүктелген.";
+    }
+
+    else if(val == "Бақылау калибрлері нені бақылайды") {
+          document.getElementById('answer').innerHTML="Ответ: Калибр –скоба";
+    }
+
+    else if(val == "Дөңгелектен ауытқудын жеке түрі:") {
+          document.getElementById('answer').innerHTML="Ответ: Қырлы және сопақтық.";
+    }
+
+    else if(val == "Қолданылуына байланысты бұрандалар қалай бөлінеді:") {
+          document.getElementById('answer').innerHTML="Ответ: Жалпы тағайындалған және арнайы бұрандалар.";
+    }
+
+    else if(val == "Дөңгелектен ауытқудын жеке түрі:") {
+          document.getElementById('answer').innerHTML="Ответ: Қырлы және сопақтық.";
+    }
+
+    else if(val == "Жазықтықтан ауытқудың жеке түрлері:") {
+          document.getElementById('answer').innerHTML="Ответ: Дөңестік және ойыстық.";
+    }
+
+    else if(val == "Жұмысшы скобаның номинальды өлшемі тең...") {
+          document.getElementById('answer').innerHTML="Ответ: Біліктің ең үлкен шекті өлшеміне.";
+    }
+
+    else if(val == "Біліктің сызбадағы диаметрі 120, қандай нақты өлшемді жарамсыз деп тану керек?") {
+          document.getElementById('answer').innerHTML="Ответ: Æ119,973";
+    }
+
+    else if(val == "Жұмысшы калибрлермен анықтау мүмкін") {
+          document.getElementById('answer').innerHTML="Ответ: Нақты өлшемдер жарамды ма немесе белгіленген шектен шыға ма";
+    }
+
+    else if(val == "Бақылау калибрлері не үшін қажет:") {
+          document.getElementById('answer').innerHTML="Ответ: Калибр-скобаны бақылау үшін.";
+    }
+
+    else if(val == "Метрлі бұранда дәлдік классы") {
+          document.getElementById('answer').innerHTML="Ответ: Дәл, орташа, дөрекі";
+    }
+
+    else if(val == "Подшипниктің дәлдік классы") {
+          document.getElementById('answer').innerHTML="Ответ: 0,6,5,4,2";
+    }
+
+    else if(val == "Қолданылуына байланысты бұрандалар қалай бөлінеді:") {
+          document.getElementById('answer').innerHTML="Ответ: Жалпы тағайындалған және арнайы бұрандалар.";
+    }
+
+    else if(val == "Жоғарғы шекті ауытқу:") {
+          document.getElementById('answer').innerHTML="Ответ: Ең жоғарғы және ең төменгі шекті ауытқулардың алгебралық айырмашылығы.";
+    }
+
+    else if(val == "Нақты өлшем дегеніміз:") {
+          document.getElementById('answer').innerHTML="Ответ: Белгіленген мүмкіндік қателікпен өлшенген өлшем";
+    }
+  }
